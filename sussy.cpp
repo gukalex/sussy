f33f1f67d45afd99302a100f9a13065a4f20b681
gukalex <guk.alex.v@gmail.com>
2025-02-11 02:05:21 +0100

    starting with parsing

--- sussy.cpp
+++ sussy.cpp
@@ -8,17 +8,21 @@ time clang sussy.cpp -std=c++17 -g -luser32.lib -Wno-deprecated-declarations -o 
 #include <stdlib.h>
 #include <string.h>
 
+
+// TYPE ALIASES START
 using u8 = unsigned char;
+using i8 = char;
 using u32 = unsigned int;
 using u64 = unsigned long long int;
 using i32 = int;
 using i64 = unsigned long long int;
 
+// COMMON STRUCTS AND FUNCTIONS START
 struct buf {
     u8* data;
     u64 size;
 };
-/* sized string macro, use as printf("as string %.*s", SS(my_buf)) */
+// sized string macro, use as printf("as string %.*s", SS(my_buf))
 #define SS(b) (int)b.size, b.data
 
 struct arena {
@@ -59,16 +63,15 @@ bool eq(buf str, buf str2) {
     return memcmp(str.data, str2.data, str.size) == 0;
 }
 
-buf read_file(buf input_file_name) {
+buf read_file(buf input_file_name, int zero_padding_bytes = 1) {
     buf f = {};
-    int zero_padding_bytes = 1;
 	FILE* file = fopen((const char*)input_file_name.data, "rb");
 	if (file != NULL) {
 		fseek(file, 0, SEEK_END);
 		f.size = ftell(file);
 		fseek(file, 0, SEEK_SET);
 
-		f.data = push(&glob, f.size + zero_padding_bytes);
+		f.data = push(&glob, f.size + zero_padding_bytes); // todo: assert temp size
 
 		u64 read_count = fread(f.data, f.size, 1, file);
         for (int i = 0; i < zero_padding_bytes; i++)
@@ -89,10 +92,87 @@ void write_file(const char* filename, buffer buf) {
 }
 */
 
+// COMPILER START
+
+bool idt_start(char c) { // is start of identifier, cannot include numbers or other symbols except '_'
+    if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '_')
+        return true;
+    return false;
+}
+
+enum token {
+    NONE, // empty
+    NAME, // identifier name, function/variable
+    COMMENT,
+    SEPARATOR, // spaces, new lines
+    END_EXP, // ';'
+};
+
+struct token_mtd {
+    buf src;
+    token t;
+};
+
+token_mtd consume(char** addr_for_c, token t) {
+    char *c = *addr_for_c;
+    token_mtd mtd = { buf{(u8*)c, 0}, t };
+    switch (t) {
+        case COMMENT:
+        // c++
+        // mtd.src = {c, diff}
+        break;
+        case NAME:
+        //...
+        default: 
+        break;
+    }
+    return mtd;
+}
+
 void comp(buf input_file_name) {
     // read the file
-    buf input = read_file(input_file_name);
+    // todo: assert utf8/ascii encoding or something
+    buf input = read_file(input_file_name, 10); /* we can read a few bytes past the end of the file and be fine */
     printf("input:\n %.*s\n", SS(input));
+    printf("input size: %d\n", (int)input.size);
+    
+    // an array of identifiers and their types
+    struct {
+        buf name;
+        buf type;
+    } idents[1000] = {}; // todo: proper sizing
+    int idents_count = 0;
+
+    // parse toplevel - get a list of identifiers
+    // todo: tokens
+    char *c = (i8*)input.data;
+    while ((u8*)c < input.data + input.size) {
+        int curr = (u64)c - (u64)input.data;
+        //printf("%d: %c (%d)\n", curr, (*c == '\n' ? '^' : *c), *c);
+        switch (*c) {
+            case ' ': case '\n': case '\r': case '\t':
+            c++; break;
+            case '/':
+                if (c[1] == '/') { // 1. can be a // comment
+                    while(*c != '\n' && *c != 0) c++;
+                    break;
+                }
+                if (c[1] == '*') { // 2. can be a /* */ comment
+                    // todo: nested comments
+                    c += 2; // revind to the char past '*'
+                    while (!(c[0] == '*' && c[1] == '/') && *c != 0) c++;
+                    c += 2; // skip past the last '/'
+                    break;
+                }
+                c++; break; // 3. can be a division in an expression
+            default:
+                if (idt_start(*c)) { 
+                    //... = consume(&c, NAME);
+                }
+                c++; break;
+        }
+        // process token for next stage -> add to token list
+    }
 }
 
 int main(int argc, char** argv) {
