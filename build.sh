da58ef65cba3b43dcc5202736fd3061193425ce2
gukalex <guk.alex.v@gmail.com>
2025-03-07 02:46:00 +0100

    *(int*)(0) = 0

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 : ''
 TIMEFORMAT="%R sec"
-time clang sussy.cpp -std=c++17 -g -luser32.lib -Wno-deprecated-declarations -o sussy.exe
+time clang sussy.cpp -std=c++17 -g -luser32.lib -Wno-deprecated-declarations -Wno-null-dereference -o sussy.exe
 if [ $? -eq 0 ]; then
     time ./sussy.exe comp test.sus
 else
